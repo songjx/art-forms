b3894117f0b59e4fdf286eb3b947682eb298f419
Mate Kovacs <mkovaxx@gmail.com>
2016-09-20 21:16:39 -0700

    use indexed geometry anyway because TWGL doesn't know any better...

--- main.js
+++ main.js
@@ -13,13 +13,8 @@ class TriangleMesh {
 
   getArrays() {
     let position = [];
-    /*
-    for (let i = 0; i < this.nodes.length; i++) {
-      let node = this.nodes[i];
-      position.push(node.pos[0], node.pos[1], node.pos[2]);
-    }
-    */
     let normal = [];
+    let indices = [];
     for (let i = 0; i < this.triangles.length; i++) {
       let triangle = this.triangles[i];
       let a = this.nodes[triangle.na].pos;
@@ -34,10 +29,14 @@ class TriangleMesh {
       normal.push(n[0], n[1], n[2]);
       normal.push(n[0], n[1], n[2]);
       normal.push(n[0], n[1], n[2]);
+      indices.push(3*i + 0);
+      indices.push(3*i + 1);
+      indices.push(3*i + 2);
     }
     return {
       position: position,
       normal: normal,
+      indices: indices,
     };
   }
 }
@@ -83,14 +82,6 @@ function makeTetrahedron() {
 var mesh = makeTetrahedron();
 
 var arrays = mesh.getArrays();
-
-/*
-var arrays = {
-    position: [1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1],
-    normal:   [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1],
-    indices:  [0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23],
-};
-*/
 
 var bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
 
